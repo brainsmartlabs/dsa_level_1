72bb3a51a94d7c94df0689d692b8df2a5288ce27
Balaji KR <brainsmartlabs@gmail.com>
2024-08-08 11:10:56 +0530

    Print prime numbers from Low to High

--- P_001_prime_numbers/src/PrimeNumberThree.java
+++ P_001_prime_numbers/src/PrimeNumberThree.java
@@ -1,4 +1,30 @@
+//Print all the Prime Numbers from Low to High
 
 public class PrimeNumberThree {
+	public static void main(String[] args) {
+		int low = 20;
+		int high = 100;
+
+		boolean flag = false;
+		System.out.println("Prime Numbers between " + low + " & " + high + " are: ");
+		
+		//outer loop
+		for (int i = low; i <= high; i++) {
+			flag = true;
+			
+			//inner loop
+			for (int div = 2; div * div <= i; div++) {
+				if (i % div == 0) {
+					flag = false;
+					break; // Optimization
+				}
+			}
+
+			if (flag == true) {
+				System.out.print(i + ",");
+			}
+		}
+
+	}
 
 }
